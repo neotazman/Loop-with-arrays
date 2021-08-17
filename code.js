a0655f3536bfb6d7912c0881fec16aadc7381b39
Tristan Alie <TristanAlie@yahoo.com>
2021-08-17 13:59:34 -0400

    added comments

--- code.js
+++ code.js
@@ -5,33 +5,33 @@ const sampleArray = [469, 755, 244, 245, 758, 450, 302, 20, 712, 71, 456, 21, 39
 
 // Your code here
 
-let basicChallenge = document.getElementById('basicChallenge');
+let basicChallenge = document.getElementById('basicChallenge'); // the element that is displaying the outputDivs
 
 let allNumbers = (input) => {
-    let outputDiv = document.createElement('p')
+    let outputDiv = document.createElement('p') //creates a line with the answers
 
     for(i = 0; i < input.length; i++) {
-        if(i === input.length - 1) {
+        if(i === input.length - 1) { //if it's the last one, there is no comma at the end
             outputDiv.append(JSON.stringify(input[i]));
         } else {
             outputDiv.append(JSON.stringify(input[i]) + ', ');
         }
     }
 
-    basicChallenge.append(outputDiv)
+    basicChallenge.append(outputDiv) //appends the string to the basic challenge div
 }
 
 let evenNumbers = (input) => {
     let outputDiv = document.createElement('p')
-    let evenNumbersArr = []
+    let evenNumbersArr = [] //creates a new array to put all the even numbers
 
-    for(i = 0; i < input.length; i++) {
+    for(i = 0; i < input.length; i++) { //filters out the odd numbers
         if(input[i] % 2 === 0) {
             evenNumbersArr.push(input[i])
         }
     }
 
-    for(i = 0; i < evenNumbersArr.length; i++) {
+    for(i = 0; i < evenNumbersArr.length; i++) { //writes the even numbers into a string
         if(i === evenNumbersArr.length - 1) {
             outputDiv.append(JSON.stringify(evenNumbersArr[i]))
         } else {
@@ -63,6 +63,9 @@ let oddNumbers = (input) => {
     basicChallenge.append(outputDiv)
 }
 
+
+
+//run the functions
 allNumbers(sampleArray)
 evenNumbers(sampleArray)
 oddNumbers(sampleArray)
